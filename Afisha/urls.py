b0596c7a04cedbcad1663a1bf121a7c77690d9e0
ToktosunovUmar <toktosunovumar66@gmail.com>
2024-10-09 23:12:50 +0600

    hw3

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -4,11 +4,11 @@ from movie_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/directors/', views.director_api_view, name='director-api'),
+    path('api/v1/directors/', views.director_create_api_view, name='director-api'),
     path('api/v1/directors/<int:id>/', views.director_detail_api_view),
-    path('api/v1/movies/', views.movie_api_view),
+    path('api/v1/movies/', views.movie_create_api_view),
     path('api/v1/movies/<int:id>/', views.movie_detail_api_view),
     path('api/v1/movies/reviews/', views.movies_with_reviews_api_view),
-    path('api/v1/reviews/', views.review_api_view),
+    path('api/v1/reviews/', views.review_create_api_view),
     path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
 ]
